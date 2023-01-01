45bbc4f86874204149e3715128e32754e8c4c7c7
shreeya <shreeya.j95@gmail.com>
2023-01-01 15:49:15 +0530

    1. Added UI for HighCare Pest Control

---
+++ HighCarePestControl/HighCareMainApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HighCareMainApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MaxHeight = (SystemParameters.MaximizedPrimaryScreenHeight);
+            this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+            this.Left = 0;
+            this.Top = 0;
+        }
+    }
+}
